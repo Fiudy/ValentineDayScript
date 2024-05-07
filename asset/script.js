c69f11ff9d05d1b75496777ed7575f397a5b14ab
Guilherme Carvalho <89918329+Fiudy@users.noreply.github.com>
2024-05-07 01:32:10 -0300

    Add files via upload

---
+++ asset/script.js
@@ -0,0 +1,69 @@
+$('.tombol').click(function() {
+    // animate content
+    $('.halaman').addClass('animate_content');
+    $('.dalemnya_halaman').fadeOut(100).delay(2800).fadeIn();
+
+    setTimeout(function() {
+        $('.halaman').removeClass('animate_content');
+    }, 3200);
+
+    //remove fadeIn class after 1500ms
+    setTimeout(function() {
+        $('.halaman').removeClass('fadeIn');
+    }, 1100);
+});
+
+
+$('.tombol2').click(function() {
+    // animate content
+    $('.halaman').addClass('animate_content');
+    $('.dalemnya_halaman').fadeOut(100).delay(2800).fadeIn();
+
+    setTimeout(function() {
+        $('.halaman').removeClass('animate_content');
+    }, 3200);
+
+    //remove fadeIn class after 1500ms
+    setTimeout(function() {
+        $('.halaman').removeClass('fadeIn');
+    }, 1100);
+
+});
+
+
+$('.kehome').click(function() {
+    setTimeout(function() {
+        $('.home').addClass('fadeIn');
+    }, 1100);
+
+    PuterLagu();
+});
+
+
+$('.kehalaman2').click(function() {
+    setTimeout(function() {
+        $('.halaman2').addClass('fadeIn');
+    }, 1100);
+
+    // Delay 4 detik sebelum memulai fungsi JalaninTeks()
+    setTimeout(JalaninTeks, 4000);
+    PuterLagu();
+});
+
+var i = 0;
+var txt = "Oi meu amor. Quero dizer que eu te amo muito, de verdade. Por uns meses ficamos separados, mas eu estou feliz de estarmos tentando de novo. Porque sinceramente, você é o único amor da minha vida. Eu sei que não sou uma pessoa muito fácil de lidar, mas eu prometo que vou tentar ser melhor. Eu te amo muito, muito mesmo. E eu quero passar o resto da minha vida ao seu lado. Eu te amo, meu amor. Eu te amo muito. E por isso eu quero que a gente alcance nossas metas e sonhos juntos.";
+var speed = 200;
+
+function JalaninTeks() {
+    if (i < txt.length) {
+        document.getElementById("tekss").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(JalaninTeks, speed);
+    }
+};
+
+
+function PuterLagu() {
+    var lagu = document.getElementById("lagu");
+    return lagu.paused ? lagu.play() : lagu.pause();
+};
